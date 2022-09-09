a2630f75a50012e52b7a74cdebb47eafc4275642
ndouishmael <ndouishmael@gmail.com>
2022-09-09 19:31:04 +0200

    my second commit

--- subscribe.c
+++ subscribe.c
@@ -5,7 +5,9 @@ int main()
 	int radius; //the distance from outside to center of the circle
 	printf("Please enter a radius: ");
 	scanf("%i", &radius); //address of operator
+	
+	double area = 3.14159 * (radius * radius);
 
-	printf("The given radius is %i\n" ,radius);
+	printf("The area of a circle with %i\n radius is %f\n" ,radius , area);
 	return 0;
 }
