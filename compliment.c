5495f24a543dab90df88be58e48c32234f89e9f7
ndouishmael <ndouishmael@gmail.com>
2022-09-09 21:26:19 +0200

    eggs app

---
+++ compliment.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int mani()
+{
+	printf("Hello Ishmael\n");
+
+	return 0;
+}
+#include <stdio.h>
+
+int main()
+{
+	char name [31];
+		printf("What is your name: ");
+		scanf("%s" , name);
+		printf("Hell0 %s!\nYou look very nice today\n" , name);
+
+	return 0;
+}
+
